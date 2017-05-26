e8c478fc295de6e2db7d1582a1f0a196d90fb681
Chris Christoudias <christos.christoudias@gmail.com>
2017-05-26 14:42:09 -0700

    build rover directionals

--- lib/mars/communication.rb
+++ lib/mars/communication.rb
@@ -1,3 +1,11 @@
+#############################################
+#  Mars::Communication - Takes a transmission, validates it and sends it to a group of rovers
+#  CGC 5/26/2017
+#
+#
+#############################################
+
+
 module Mars
   class Communication
 
@@ -5,6 +13,12 @@ module Mars
     def initialize(transmission = nil)
       @transmission = transmission
       @transmission_errors = []
+      @rovers = [] # array of rover transmissions. Each rover transmission is an array of 2
+      @orientations = ["N", "E", "W", "S"]
+      @moves = ["M", "L", "R"]
+      @response = [] # the response from the rovers.
+      @maxx = 0
+      @maxy = 0
     end
 
 
@@ -12,10 +26,18 @@ module Mars
       cleanup_transmission
       validate_total_lines
       validate_plateau
+
+      # At this point in validation, we can ignore the first line and parse the rest of the array in to subarray pairs
+      # We store the array pairs in the @rovers array
       if !@transmission_errors.present?
+        rovers_t = @transmission
+        rovers_t.delete_at(0)
+        @rovers = rovers_t.each_slice(2).to_a
+        @rovers.each do |rover|
+          validate_rover(rover)
+        end
 
       end
-
       !@transmission_errors.present?
     end
 
@@ -32,7 +54,7 @@ module Mars
 
     def validate_total_lines
       if @transmission.size % 2 != 1
-        @transmission_errors << "There should be an odd number of lines."
+        @transmission_errors << "There should be an odd number of lines"
       end
     end
 
@@ -40,30 +62,60 @@ module Mars
     def validate_plateau
       plateau = @transmission[0].split(" ")
       if plateau.size != 2
-        @transmission_errors << "First line should have two values representing the X and Y of the plateau."
+        @transmission_errors << "First line should have two values representing the X and Y of the plateau"
+      elsif !((Integer(plateau[0]) rescue nil) and (Integer(plateau[1]) rescue nil))
+        @transmission_errors << "First line should have numbers for first two values"
       else
-        if !((Integer(plateau[0]) rescue nil) and (Integer(plateau[1]) rescue nil))
-          @transmission_errors << "First line should have numbers for first two values."
+        @maxx = plateau.first.to_i
+        @maxy = plateau.last.to_i
+      end
+    end
+
+
+    # validates the instructions sent to each rover.
+    def validate_rover(rover_transmission = [])
+      if rover_transmission.size != 2
+        @transmission_errors << "#{rover_transmission.to_s} should be two lines"
+      else
+        first_line = rover_transmission.first.split(" ")
+        if first_line.size != 3
+          @transmission_errors << "First line of #{rover_transmission.to_s} should have three entries"
+        elsif !(Integer(first_line.first) rescue nil) and (Integer(first_line[1]) rescue nil)
+          @transmission_errors << "First two entries of #{rover_transmission.first.to_s} should be integers"
+        else
+          startx = Integer(first_line.first)
+          starty = Integer(first_line[1])
+          if startx > @maxx or startx < 0 or starty > @maxy or starty < 0
+            @transmission_errors << "Starting position of #{rover_transmission.first.to_s} must be in bounds."
+          end
+          if !@orientations.include?(first_line.last)
+            @transmission_errors << "Last entry of #{rover_transmission.first.to_s} should be a direction"
+          end
+          #@transmission_errors << "#{rover_transmission[1].gsub("M", "")}"
+          if rover_transmission.last.gsub("M", "").gsub("R","").gsub("L", "").size > 0
+            @transmission_errors << "#{rover_transmission.last.to_s} should only contain L, M, or R characters"
+          end
         end
       end
     end
 
-    
-    def validate_instructions
+
+    def send_to_rovers
+      @rovers.each do |rover|
+        @response << Mars::Rover.new(@maxx, @maxy, rover).run
+      end
+
 
     end
 
 
-
-
     def send_transmission
       if validate_transmission
-        "GOOD Transmission"
+        send_to_rovers
+        @response.join("\n")
       else
         "TRANSMISSION FAILURE: #{@transmission_errors.join(",")}"
       end
-
-
     end
 
 
--- lib/mars/rover.rb
+++ lib/mars/rover.rb
@@ -1,10 +1,92 @@
+#############################################
+#  Mars::Rover - Takes a rovers transmission and moves the rover. Returns the final position and orientation
+#  CGC 5/26/2017
+#
+#
+#############################################
+
 module Mars
+
+
   class Rover
 
-    def initialize(instructions = nil)
+    def initialize(maxx = 0, maxy =0, rover = ["",""])
+      first_line = rover.first.split(" ")
+      @instructions = rover.last
+      @orientation = first_line.last
+      @positionx = first_line.first.to_i
+      @positiony = first_line[1].to_i
+      @maxx = maxx
+      @maxy = maxy
+    end
 
+    def response
+      "#{@positionx} #{@positiony} #{@orientation}"
+    end
+
+    def run
+      @instructions.split("").each do |i|
+        move(i)
+      end
+      response
+    end
+
+    def move(instruction)
+      case instruction
+        when "M"
+          advance
+        when "R"
+          turn_right
+        when "L"
+          turn_left
+      end
+    end
+
+
+    def turn_right
+      case @orientation
+        when "N"
+          @orientation = "E"
+        when "E"
+          @orientation = "S"
+        when "S"
+          @orientation = "W"
+        when "W"
+          @orientation = "N"
+      end
+    end
+
+
+    def turn_left
+      case @orientation
+        when "N"
+          @orientation = "W"
+        when "W"
+          @orientation = "S"
+        when "S"
+          @orientation = "E"
+        when "E"
+          @orientation = "N"
+      end
+    end
+
+
+    def advance
+      case @orientation
+        when "N"
+          @positiony += 1 unless @positiony >= @maxy
+        when "W"
+          @positionx -= 1 unless @positionx <= 0
+        when "S"
+          @positiony -= 1 unless @positiony <= 0
+        when "E"
+          @positionx += 1 unless @positionx >= @maxx
+
+      end
     end
 
 
   end
+
+
 end
